aafd3e46fe293f728c1a00b34d238521f78e85d9
Ty Eggen <tycho.eggen@gmail.com>
2024-05-06 22:56:05 -0700

    feat(prettier-config-dct): 🎸 initial version

---
+++ packages/prettier-config-dct/prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    semi: false,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    trailingComma: 'none',
+    bracketSpacing: true,
+    bracketSameLine: false,
+    arrowParens: 'always',
+    proseWrap: 'preserve',
+    htmlWhitespaceSensitivity: 'css',
+    embeddedLanguageFormatting: 'auto',
+    singleAttributePerLine: false,
+    plugins: ['prettier-plugin-sh']
+}
