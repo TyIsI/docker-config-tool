fe5066f5eb3ecdfd3390c173192609d5e5d424d6
Ty Eggen <tycho.eggen@gmail.com>
2024-06-05 13:38:55 -0700

    test(docker-config-tool-js): 💍 improved stage testing

--- packages/docker-config-tool-js/src/lib/stage/__tests__/class/methods/forceRandomId.ts
+++ packages/docker-config-tool-js/src/lib/stage/__tests__/class/methods/forceRandomId.ts
@@ -5,13 +5,12 @@ describe(`DCT`, () => {
         describe(`stage`, () => {
             describe(`class`, () => {
                 describe(`methods`, () => {
-                    describe(`setId`, () => {
-                        test(`create a stage from string and set id`, () => {
+                    describe(`forceRandomId`, () => {
+                        test(`forceRandomId`, () => {
                             const stage = new Stage('scratch')
 
-                            stage.setId('base')
-
-                            expect(stage.toString()).toMatch('FROM scratch AS base')
+                            // @ts-expect-error private
+                            expect(stage.forceRandomId()).toMatch(/stage-\w+/)
                         })
 
                         test(`throw an error if trying to set invalid id`, () => {
@@ -22,18 +21,6 @@ describe(`DCT`, () => {
 
                                 // @ts-expect-error invalid
                                 stage.setId(testVal)
-                            }).toThrow()
-                        })
-
-                        test(`throw an error if trying to set id and first instruction is not FROM`, () => {
-                            expect(() => {
-                                const stage = new Stage('scratch')
-
-                                stage.withArg('TESTARG')
-
-                                stage.stack.splice(0, 1)
-
-                                stage.setId('testVal')
                             }).toThrow()
                         })
                     })
